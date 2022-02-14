90b58da133a573cfdc54735aa0eca8cc74ed0934
Gabriel Paschoal <gpaschoal@MMB027553SPO.local>
2022-02-14 10:54:20 -0300

    Refactor Api connect component

--- MusicFetcher/MusicFetcher/ContentView.swift
+++ MusicFetcher/MusicFetcher/ContentView.swift
@@ -1,54 +1,49 @@
 import SwiftUI
 
 struct ContentView: View {
-  
-  @State private var musicItems: [MusicItem] = []
-  
-  var body: some View {
-    VStack {
-      Button(action: fetchMusic) {
-        Text("Fetch Music")
-      }
-      List(musicItems) { item in
-        Text(item.trackName)
-      }
-      Spacer()
-    }
-  }
-  
-  func fetchMusic() {
-    guard let url =  URL(string:"https://itunes.apple.com/search?media=music&entity=song&term=cohen") else {
-      return
+    
+    @State private var musicItems: [MusicItem] = []
+    
+    var body: some View {
+        VStack {
+            Button(action: fetchMusic) {
+                Text("Fetch Music")
+            }
+            List(musicItems) { item in
+                Text(item.trackName)
+            }
+            Spacer()
+        }
     }
     
-      let configuration = URLSessionConfiguration.default
-      let session = URLSession(configuration: configuration)
-     
-      let task = session.dataTask(with: url) {
-          data, response, error in
-          
-          guard let data = data else {
-              return
-          }
-          
-          let decoder = JSONDecoder()
-          
-          guard let response = try? decoder.decode(MediaResponse.self, from: data) else {
+    func fetchMusic() {
+        guard let url =  URL(string:"https://itunes.apple.com/search?media=music&entity=song&term=cohen") else {
             return
-          }
-          
-          DispatchQueue.main.async {
-            self.musicItems = response.results
-          }
-      }
-      
-      task.resume()
-  }
-}
-
-
-struct ContentView_Previews: PreviewProvider {
-  static var previews: some View {
-      ContentView()
-  }
+        }
+        URLSession.shared.dataTask(with: url) {
+            data, response, error in
+            guard let httpResponse = response as? HTTPURLResponse,
+                  (200..<300).contains(httpResponse.statusCode),
+                  let data = data else {
+                      fatalError()
+                  }
+            
+            let decoder = JSONDecoder()
+            
+            guard let response = try? decoder.decode(MediaResponse.self, from: data) else {
+                return
+            }
+            
+            DispatchQueue.main.async {
+                self.musicItems = response.results
+            }
+            
+        }.resume()
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
 }
